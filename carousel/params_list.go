21d5800462920d63afcc58873446455642588297
Intak Hwang <sp301415@gmail.com>
2025-05-20 01:46:13 +0000

    fix: Update parameters

--- carousel/params_list.go
+++ carousel/params_list.go
@@ -1,25 +1,99 @@
 package carousel
 
+import "math"
+
 var (
-	// ParamsSlotsUint2 is the parameters for carousel-slots with 2 bits of message space.
-	ParamsSlotsUint2 = ParametersLiteral{
-		LWEDimension: 628,
+	// ParamsCoeffsUint2 is the parameters for carousel-coeffs with 2 bits of message space.
+	ParamsCoeffsUint2 = ParametersLiteral{
+		LWEDimension: 687,
 		PolyDegree:   2048,
 
-		LWEStdDev:  1090715534753795.8,
-		RLWEStdDev: 6148,
+		LWEStdDev:  math.Exp2(49.07),
+		RLWEStdDev: math.Exp2(13.05),
 
-		BlockSize: 2,
+		BlockSize: 3,
 
 		MessageModulus: 1 << 2,
 
 		BlindRotateParameters: GadgetParametersLiteral{
-			Base:  1 << 13,
-			Level: 2,
+			Base:  1 << 20,
+			Level: 1,
+		},
+		KeySwitchParameters: GadgetParametersLiteral{
+			Base:  1 << 2,
+			Level: 6,
+		},
+
+		EncodeType: EncodeTypeCoeffs,
+	}
+
+	// ParamsCoeffsUint3 is the parameters for carousel-coeffs with 3 bits of message space.
+	ParamsCoeffsUint3 = ParametersLiteral{
+		LWEDimension: 788,
+		PolyDegree:   2048,
+
+		LWEStdDev:  math.Exp2(46.60),
+		RLWEStdDev: math.Exp2(13.09),
+
+		BlockSize: 4,
+
+		MessageModulus: 1 << 3,
+
+		BlindRotateParameters: GadgetParametersLiteral{
+			Base:  1 << 22,
+			Level: 1,
+		},
+		KeySwitchParameters: GadgetParametersLiteral{
+			Base:  1 << 4,
+			Level: 3,
+		},
+
+		EncodeType: EncodeTypeCoeffs,
+	}
+
+	// ParamsCoeffsUint4 is the parameters for carousel-coeffs with 4 bits of message space.
+	ParamsCoeffsUint4 = ParametersLiteral{
+		LWEDimension: 828,
+		PolyDegree:   2048,
+
+		LWEStdDev:  math.Exp2(45.57),
+		RLWEStdDev: math.Exp2(13.09),
+
+		BlockSize: 4,
+
+		MessageModulus: 1 << 4,
+
+		BlindRotateParameters: GadgetParametersLiteral{
+			Base:  1 << 23,
+			Level: 1,
 		},
 		KeySwitchParameters: GadgetParametersLiteral{
 			Base:  1 << 3,
-			Level: 4,
+			Level: 5,
+		},
+
+		EncodeType: EncodeTypeCoeffs,
+	}
+
+	// ParamsSlotsUint2 is the parameters for carousel-slots with 2 bits of message space.
+	ParamsSlotsUint2 = ParametersLiteral{
+		LWEDimension: 687,
+		PolyDegree:   2048,
+
+		LWEStdDev:  math.Exp2(49.07),
+		RLWEStdDev: math.Exp2(13.05),
+
+		BlockSize: 3,
+
+		MessageModulus: 1 << 2,
+
+		BlindRotateParameters: GadgetParametersLiteral{
+			Base:  1 << 14,
+			Level: 2,
+		},
+		KeySwitchParameters: GadgetParametersLiteral{
+			Base:  1 << 2,
+			Level: 6,
 		},
 
 		EncodeType: EncodeTypeSlots,
@@ -27,19 +101,19 @@ var (
 
 	// ParamsSlotsUint3 is the parameters for carousel-slots with 3 bits of message space.
 	ParamsSlotsUint3 = ParametersLiteral{
-		LWEDimension: 687,
+		LWEDimension: 788,
 		PolyDegree:   2048,
 
-		LWEStdDev:  373833953443844,
-		RLWEStdDev: 6148,
+		LWEStdDev:  math.Exp2(46.60),
+		RLWEStdDev: math.Exp2(13.09),
 
-		BlockSize: 3,
+		BlockSize: 4,
 
 		MessageModulus: 1 << 3,
 
 		BlindRotateParameters: GadgetParametersLiteral{
-			Base:  1 << 10,
-			Level: 3,
+			Base:  1 << 16,
+			Level: 2,
 		},
 		KeySwitchParameters: GadgetParametersLiteral{
 			Base:  1 << 4,
@@ -51,23 +125,23 @@ var (
 
 	// ParamsSlotsUint4 is the parameters for carousel-slots with 4 bits of message space.
 	ParamsSlotsUint4 = ParametersLiteral{
-		LWEDimension: 717,
+		LWEDimension: 828,
 		PolyDegree:   2048,
 
-		LWEStdDev:  215504279044099.94,
-		RLWEStdDev: 6148,
+		LWEStdDev:  math.Exp2(45.57),
+		RLWEStdDev: math.Exp2(13.09),
 
-		BlockSize: 3,
+		BlockSize: 4,
 
 		MessageModulus: 1 << 4,
 
 		BlindRotateParameters: GadgetParametersLiteral{
-			Base:  1 << 8,
-			Level: 4,
+			Base:  1 << 17,
+			Level: 2,
 		},
 		KeySwitchParameters: GadgetParametersLiteral{
 			Base:  1 << 3,
-			Level: 4,
+			Level: 5,
 		},
 
 		EncodeType: EncodeTypeSlots,
